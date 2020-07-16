688220ccc9312d2db0da147612c74744b5d092a8
Arin <arinyazilim@gmail.com>
2020-07-16 09:51:44 +0300

    Der 5

---
+++ 05_variable_declaration/main.go
@@ -0,0 +1,19 @@
+// Package clause
+package main
+
+// Import statement
+import "fmt"
+
+// My Code
+func main() {
+
+	name := "Arin"
+	age := 40
+	isMarried := true
+
+	age = 41
+
+	fmt.Println(name)
+	fmt.Println(age)
+	fmt.Println(isMarried)
+}
