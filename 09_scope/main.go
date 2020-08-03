d7243e2dc35274fda4ec77384200d31c62562609
Arin <arinyazilim@gmail.com>
2020-08-03 12:32:27 +0300

    Ders 09

---
+++ 09_scope/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+/* var packVar = "Package Scope"
+var funcVar = "Func(Package) Scope" */
+
+func main() {
+
+	/* 	if true {
+		var blockVar = "Block Scope"
+		fmt.Println(blockVar)
+	} */
+
+	var name = "Arin"
+	name, surname := "Elis", "Software"
+	fmt.Println(name, surname)
+
+	/* 	fmt.Println(packVar)
+
+	   	myFunc() */
+
+}
+
+/* func myFunc() {
+	fmt.Println(funcVar)
+}
+*/
