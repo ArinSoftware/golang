8400fa5ef4faba83e14f02be6f69c05cde4c767a
Arin <arinyazilim@gmail.com>
2020-08-11 11:38:13 +0300

    Ders 11

---
+++ 11_naming_print/main.go
@@ -0,0 +1,63 @@
+package main
+
+/* var x = 10
+
+var customerName
+
+var Y = 100 */
+
+func main() {
+
+	// (Print - Println) - Printf
+
+	/* 	{
+		var c
+	} */
+
+	/* 	fmt.Println("Merhaba")
+	   	fmt.Print("Merhaba")
+	   	fmt.Println("")
+	   	fmt.Printf("Merhaba") */
+
+	/* 	name := "Arin" */
+
+	/* 	fmt.Print(name)
+	   	fmt.Println(name)
+	   	fmt.Printf(name) */
+
+	/* 	fmt.Print("Benim Adım", name)
+	   	fmt.Println("")
+	   	fmt.Println("Benim Adım", name)
+	   	fmt.Printf("Benim Adım %X %T", name, name) */
+
+	/* 	x := 100
+	   	y := 20
+	   	z := 30
+
+	   	fmt.Printf("%b %d %o", x, y, z) */
+
+	/* name, age := "Arin", 5 */
+
+	// fmt.Print("Benim Adım ", name, ", ve ben ", age, " yaşındayım.")
+	// fmt.Println("Benim Adım", name, "ve ben", age, "yaşındayım.")
+	// fmt.Printf("Benim Adım %v, ve ben %v yaşındayım", name, age)
+
+	// VISIBILITY
+
+	/* 	fmt.Println(x)
+
+	   	myFunc() */
+
+	/* 	var coin string  // count - customer - coin
+	   	// Go camel case isimlendirme kullanılır
+	   	var coinType string
+	   	var custName string
+	   	// kısaltmalar büyük harflerle yazılır
+	   	var URL // Url değil
+	   	var HTTP // http değil "xyzHTTP"
+
+	   	i , j , k */
+
+	// benForDongusuDegiskeninİsmiyim
+
+}
