443ad42e31c08ce8595b41ee2ce21937b29c93bb
Arin <arinyazilim@gmail.com>
2020-08-18 11:27:06 +0300

    Ders 13

---
+++ 13_arithmetic_operations/main.go
@@ -0,0 +1,38 @@
+// addition + => 15 + 10 => 15, 10 operand, + operator
+// substruction -
+// product *
+// division /
+// remainder %
+
+package main
+
+import "fmt"
+
+func main() {
+	//x, y := 15, 10
+
+	//fmt.Printf("%T, %v\n", x, x)
+	//fmt.Printf("%T, %v\n", y, y)
+	//fmt.Printf("%T, %v\n", (x + y), (x + y))
+	//fmt.Printf("%T, %v\n", (x - y), (x - y))
+	//fmt.Printf("%T, %v\n", (x * y), (x * y))
+	//fmt.Printf("%T, %v\n", (9.0 / 3), (9.0 / 3))
+	//fmt.Printf("%T, %v\n", (x % y), (x % y))
+
+	//z := 5.0 / 2 // (2.5)
+	//fmt.Printf("%T, %v\n", z, z)
+
+	// Increment ++, Decrement -- POSTFIX
+
+	x := 10
+
+	fmt.Println(x)
+
+	x = x - 1
+
+	fmt.Println(x)
+
+	x--
+
+	fmt.Println(x)
+}
